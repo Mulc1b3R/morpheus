7f43355f2aafa07e99307b7647eb3f6fe97897c4
psico-mulciber <158625834+Mulc1b3R@users.noreply.github.com>
2024-04-28 16:38:26 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ const description =
   'A fully open-source AI-powered answer engine with a generative UI.'
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://morphic.sh'),
+  metadataBase: new URL('https://morpheus-cyan.vercel.app/'),
   title,
   description,
   openGraph: {
